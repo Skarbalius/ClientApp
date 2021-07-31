d06d30715c06e23bc6f3196212db00d0977ce40c
Skarbalius <48081382+Skarbalius@users.noreply.github.com>
2021-07-31 12:28:29 +0300

    Add files via upload

---
+++ ClientApp/dbo.ClientsAudit.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ClientsAudit] (
+    [Id]         INT        IDENTITY (1, 1) NOT NULL,
+    [AuditData]  NCHAR (60) NULL,
+    [RecordedAt] DATETIME   DEFAULT (getdate()) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
